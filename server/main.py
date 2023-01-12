dbc6d616c732510d5dcd7f548086b34920f1e2b6
Cory Rodriguez <coreyreylp@gmail.com>
2023-01-12 10:58:34 -0600

    Add some test data

--- server/main.py
+++ server/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+import json, os
+
+
+f = open(os.path.join("data.json"), "r")
+data = json.load(f)
+f.close()
 
 app = FastAPI()
 
@@ -9,3 +15,7 @@ def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/wells")
+def well_index():
+    return {"wells": data}
